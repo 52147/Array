782141d881a0cd61d7dda012de3ad6783ced33cd
Your Name <debra4117@gmail.com>
2022-02-25 19:02:26 +0800

    update

---
+++ src/arrayrelatedtechnique/RotateArray189.java
@@ -0,0 +1,92 @@
+package arrayrelatedtechnique;
+
+/**
+ * = Array-related Techniques =
+ * 
+ * - There are more array-related data structures or techniques you might want to know.
+ * 
+ * 1. There are some other data structures which are similar to the array but have some different properties:
+ * 
+ *   - String
+ *   - Hash Table
+ *   - Linked List
+ *   - Queue
+ *   - Stack
+ *   
+ * 2. 
+ * - As we mentioned, we can call the build-in function to sort an array.
+ * - But it is useful to understand the principle of some widely-used sorting algorithms and their complexity.
+ * 
+ * 3.
+ * - Binary search is also an important technique used to search a specific element in a sorted array.
+ * 
+ * 4.
+ * - We have introduced two-pointer technique in this chapter.
+ * - It is not easy to use this technique flexibly.
+ * - This technique can also be used to solve:
+ * 
+ *   - Slow-pointer and fast-pointer problem in Linked List
+ *   - Sliding Window Problem
+ *   
+ * 5. The two-pointer technique sometimes will relate to Greedy Algorithm which helps us design our pointers' movement strategy.
+ * 
+ *  
+ * 
+ * 
+ *
+ */
+
+
+/**
+ * 
+ * 189. Rotate Array
+ * 
+ * Approach 3: Using Cyclic Replacements
+ * 
+ * ex:
+ * 
+ * k = 2
+ * | 1 | 2 | 3 | 4 | 5 |
+ * 
+ * 
+ * | 4 | 5 | 1 | 2 | 3 |
+ * 
+ *  => this 3(1, 2, 3) element each need to move k % nums.length = 2 space (not k moves because may be k larger than array length)
+ *  
+ *     => the shifted index = (original index + 2) % length 
+ *     => index 1 shifted to new index: (0 + 2) % 5 = 2
+ *     => ...
+ *     => index 5 shifted to new index: (4 + 2) % 5 = 1
+ *    
+ *  
+ * 
+ */
+
+public class RotateArray189 {
+	
+	public void rotate(int[] nums, int k) {
+		
+		k = k % nums.length; // how many index need to move
+		int count = 0;
+		
+		for(int start = 0; count < nums.length; start++) {
+			
+			int current = start;
+			int prev = nums[start];
+			
+			do{
+				int next = (current + k) % nums.length; // shifted index = (original index + k) % len
+				
+				int temp = nums[next]; // save the element in the position where we need to put the index i element
+				nums[next] = prev; // shifted the element to the new place
+				prev = temp;  // save the old element in the prev because we need to find the shifted index for this element  
+				
+				current = next; // set the next index in the current 
+				                // because next iteration we want to find the new shifted index for the old element in this index
+				count++; // plus the count => when the while loop is over -> the for loop ended too
+				
+			}while(start != current); // if return to the index 0 stop
+		}
+	}
+
+}
--- src/twopointertechnique_2/MinimumSizeSubarraySum209.java
+++ src/twopointertechnique_2/MinimumSizeSubarraySum209.java
@@ -1,25 +1,24 @@
 package twopointertechnique_2;
 
 public class MinimumSizeSubarraySum209 {
-	
+
 	public int minSubArrayLen(int target, int[] nums) {
-		
+
 		int n = nums.length;
 		int ans = Integer.MAX_VALUE;
-		
+
 		int left = 0;
-		int right = 0;
-		int sum  = 0;
-		
-		for(int i = 0; i < n; i++) {
+		int sum = 0;
+
+		for (int i = 0; i < n; i++) {
 			sum += nums[i];
-			while(sum >= target) {
-				ans = Math.min(ans, i + 1 -left);
+			while (sum >= target) {
+				ans = Math.min(ans, i + 1 - left);
 				sum -= nums[left++];
 			}
 		}
-		
-		return (ans != Integer.MAX_VALUE) ? ans : 0;
+
+		return (ans != Integer.MAX_VALUE) ? ans : 0; // if the answer = max value means we don have the sub array
 	}
 
 }
